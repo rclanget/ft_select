3becd25157dd8c7ef8ffa8e2945aea09cec82af2
Umi LEFEBVRE <ulefebvr@e1r1p8.42.fr>
2015-09-21 15:45:17 +0200

    parsing using libft

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,17 +6,14 @@
 /*   By: ulefebvr <ulefebvr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/09/21 14:49:48 by ulefebvr          #+#    #+#             */
-/*   Updated: 2015/09/21 15:40:05 by ulefebvr         ###   ########.fr       */
+/*   Updated: 2015/09/21 15:44:49 by ulefebvr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_select.h"
+#include "libft.h"
 
 #include <stdlib.h>
-
-// #include <string.h>
-// #include <stdio.h>
-// #include <unistd.h>
 
 t_list	*create_list(char **argv, int start, t_list *prev)
 {
@@ -25,7 +22,7 @@ t_list	*create_list(char **argv, int start, t_list *prev)
 	if (!*argv)
 		return (NULL);
 	elem = malloc(sizeof(t_list));
-	elem->file = strdup(*argv);
+	elem->file = ft_strdup(*argv);
 	elem->start = start;
 	elem->prev = prev;
 	elem->next = create_list(++argv, 0, elem);
@@ -63,28 +60,3 @@ void	free_list(t_list *list)
 	free(list->file);
 	free(list);
 }
-
-// void	show_list(t_list *list)
-// {
-// 	t_list *cpy;
-
-// 	cpy = list;
-// 	while (cpy)
-// 	{
-// 		printf("%s\n", cpy->file);
-// 		cpy = cpy->next;
-// 		if (cpy->start)
-// 			break ;
-// 	}
-// }
-
-// int main(int ac, char **av)
-// {
-// 	t_list *list;
-
-// 	list = ft_parse(av);
-// 	show_list(list);
-// 	free_list(list);
-// 	sleep(10);
-// 	return 0;
-// }
