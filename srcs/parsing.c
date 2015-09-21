5bc3a2e0c45a2ac4ff90f1fc3f13b2606ab72f35
Umi LEFEBVRE <ulefebvr@e1r1p8.42.fr>
2015-09-21 15:40:37 +0200

    parsing done

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: ulefebvr <ulefebvr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/09/21 14:49:48 by ulefebvr          #+#    #+#             */
-/*   Updated: 2015/09/21 15:17:53 by ulefebvr         ###   ########.fr       */
+/*   Updated: 2015/09/21 15:40:05 by ulefebvr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 
 #include <stdlib.h>
 
-#include <string.h>
-#include <stdio.h>
+// #include <string.h>
+// #include <stdio.h>
+// #include <unistd.h>
 
 t_list	*create_list(char **argv, int start, t_list *prev)
 {
@@ -31,43 +32,59 @@ t_list	*create_list(char **argv, int start, t_list *prev)
 	return (elem);
 }
 
+t_list	*circular(t_list *list)
+{
+	t_list	*end;
+
+	end = list;
+	while (end->next)
+		end = end->next;
+	list->prev = end;
+	end->next = list;
+	return (list);
+}
+
 t_list	*ft_parse(char **argv)
 {
 	if (!argv || !*argv || !**argv)
 		return (NULL);
-	return (create_list(argv));
+	return (circular(create_list(argv, 1, NULL)));
 }
 
 void	free_list(t_list *list)
 {
-	t_list *tmp;
-
-	while (list)
+	while ((list = list->next))
 	{
-		tmp = list->next;
+		if (list->start)
+			break ;
+		free(list->file);
 		free(list);
-		list = tmp;
 	}
+	free(list->file);
+	free(list);
 }
 
-void	show_list(t_list *list)
-{
-	t_list *cpy;
+// void	show_list(t_list *list)
+// {
+// 	t_list *cpy;
 
-	cpy = list;
-	while (cpy)
-	{
-		printf("%s\n", cpy->file);
-		cpy = cpy->next;
-	}
-}
+// 	cpy = list;
+// 	while (cpy)
+// 	{
+// 		printf("%s\n", cpy->file);
+// 		cpy = cpy->next;
+// 		if (cpy->start)
+// 			break ;
+// 	}
+// }
 
-int main(int ac, char **av)
-{
-	t_list *list;
+// int main(int ac, char **av)
+// {
+// 	t_list *list;
 
-	list = ft_parse(av);
-	show_list(list);
-	free_list(list);
-	return 0;
-}
+// 	list = ft_parse(av);
+// 	show_list(list);
+// 	free_list(list);
+// 	sleep(10);
+// 	return 0;
+// }
