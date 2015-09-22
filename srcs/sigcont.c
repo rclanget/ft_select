49b85ca7fdf56c472b8f44616b4b6a97152082f4
ulefebvr <ulefebvr@student.42.fr>
2015-09-22 16:01:14 +0200

    sigcont

--- srcs/sigcont.c
+++ srcs/sigcont.c
@@ -6,7 +6,7 @@
 /*   By: ulefebvr <ulefebvr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/09/22 13:25:32 by ulefebvr          #+#    #+#             */
-/*   Updated: 2015/09/22 13:27:56 by ulefebvr         ###   ########.fr       */
+/*   Updated: 2015/09/22 16:00:59 by ulefebvr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,23 @@
 
 #include <signal.h>
 
+void	get_continue(int i)
+{
+	char			*name_term;
+	struct termios	term;
+
+	(void)i;
+	if ((name_term = getenv("TERM")) == NULL)
+		return (-1);
+	tgetent(NULL, name_term);
+	if (tcgetattr(0, &term) == -1)
+		return (-1);
+	get_canon(term);
+	get_new_size();
+}
+
+
 void	handle_sigcont(void)
 {
-	signal(SIGCONT, get_new_size);
+	signal(SIGCONT, get_continue);
 }
