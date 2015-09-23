355d9e7eb35770747195766792e28d78bc7f3e83
Umi LEFEBVRE <ulefebvr@student.42.fr>
2015-09-23 20:06:10 +0200

    all signals handled in signal.c

--- srcs/signal.c
+++ srcs/signal.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   winch_signal.c                                     :+:      :+:    :+:   */
+/*   signal.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ulefebvr <ulefebvr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/09/21 17:24:20 by ulefebvr          #+#    #+#             */
-/*   Updated: 2015/09/22 11:17:13 by ulefebvr         ###   ########.fr       */
+/*   Updated: 2015/09/23 20:04:38 by ulefebvr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,44 @@
 
 #include <signal.h>
 
-void		get_new_size(int i)
+void	handle_sigcont(int sig)
+{
+	if (sig == SIGCONT)
+		ft_init();
+	else
+	{
+		set_term(ft_glob(NULL)->sauv);
+		signal(SIGTSTP, SIG_DFL);
+		// ioctl(0, TIOCSTI, "\032");
+	}
+}
+
+void		handle_winch(int i)
 {
 	(void)i;
 	get_size();
 	print_elem(ft_glob(NULL)->list);
 }
 
-void		handle_winch(void)
+void		handling_quit(int i)
 {
-	signal(SIGWINCH, get_new_size);
+	set_term(ft_glob(NULL)->sauv);
+	ft_exit();
+}
+
+int			handle_signal(void)
+{
+	int i;
+
+	i = 0;
+	while (++i < 32)
+	{
+		if (i == SIGCONT || i == SIGTSTP)
+			signal(i, hangle_sigcont);
+		else if (i == SIGWINCH)
+			signal(i, handle_winch);
+		else if (i != SIGURG && i != SIGCHLD && i != SIGTTIN
+			&& i != SIGTTOU && i != SIGIO)
+			signal(i, handling_quit);
+	}
 }
